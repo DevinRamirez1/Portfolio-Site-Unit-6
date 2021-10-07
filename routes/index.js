aef6a18ae1188e43ff0f009205fa529fd73a254f
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-10-07 05:43:20 -0500

    Update index.js
    updating routes get. Fixing error in get for projects

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/about', (req, res) => {
 });
 
 //Get projects
-router.get('/projects/:id', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
     const projectId = req.params.id;
     const project = projects.find( ({ id }) => id === +projectId);
 
@@ -22,8 +22,8 @@ router.get('/projects/:id', function(req, res, next) {
         res.render('project', { projects } );
     } else {
         const err = new Error();
-        err.message = "Yikes, looks like there is something wrong with the server."
-        err.status = 500;
+        err.status = 404;
+        err.message = "Uh-oh, looks like the project you were looking for does not exist!"
         next(err);
     }
 });
