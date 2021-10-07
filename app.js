8dae9d63c0d6321b05ce5e9030d0cdb85e16a97f
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-10-06 19:12:40 -0500

    Update app.js
    Add router to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Required sources
 const express = require('express');
+const routes = require('./routes/index');
 
 
 //Setting view engine
@@ -7,3 +8,6 @@ app.set('view engine', 'pug');
 
 //Setting up static middleware
 app.use('/static', express.static('public'));
+
+//Router
+app.use('/', routes);
