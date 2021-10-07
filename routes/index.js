04c19b0658592a69bbe12498cd8bfa234245c8a6
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-10-06 21:49:06 -0500

    /projects/:id
    trying to projects to render

--- routes/index.js
+++ routes/index.js
@@ -14,12 +14,12 @@ router.get('/about', (req, res) => {
 });
 
 //Get projects
-router.get('/projects/id:', (req, res, next) => {
+router.get('/projects/:id', function(req, res, next) {
     const projectId = req.params.id;
     const project = projects.find( ({ id }) => id === +projectId);
 
     if (project) {
-        res.render('project', { projects, id: projectId } )
+        res.render('project', { projects } );
     } else {
         const err = new Error();
         err.message = "Yikes, looks like there is something wrong with the server."
