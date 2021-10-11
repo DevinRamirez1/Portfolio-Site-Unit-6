120156f8bf9048ba6a5046ca14312bf5cc536781
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-10-11 16:52:49 -0500

    Code updated

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const { projects } = require('../data/data.json');
 const router = express.Router();
 
 // GET home page
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res,) {
     res.render('index', { projects });
   });
 
